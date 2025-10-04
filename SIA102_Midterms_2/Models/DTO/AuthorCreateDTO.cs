fb8c30fbc5a895a1294d6ff459e803f5d04beb64
Haelyeoj Tecson <haelyeoj.tecson.college@ustangelicum.edu.ph>
2025-10-04 13:57:49 +0800

    Authors feel validated

--- SIA102_Midterms_2/Models/DTO/AuthorCreateDTO.cs
+++ SIA102_Midterms_2/Models/DTO/AuthorCreateDTO.cs
@@ -1,15 +1,37 @@
-﻿namespace SIA102_Midterms_2.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SIA102_Midterms_2.DTOs
 {
     public class AuthorCreateDTO
     {
+        [Required(ErrorMessage = "Author ID is required.")]
+        [StringLength(11, ErrorMessage = "Author ID cannot be longer than 11 characters.")]
         public string AuId { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        [StringLength(50, ErrorMessage = "Last name cannot be longer than 50 characters.")]
         public string AuLname { get; set; }
+
+        [Required(ErrorMessage = "First name is required.")]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         public string AuFname { get; set; }
+
+        [Phone(ErrorMessage = "Invalid phone number format.")]
+        [StringLength(20, ErrorMessage = "Phone number cannot be longer than 20 characters.")]
         public string Phone { get; set; }
+
+        [StringLength(100, ErrorMessage = "Address cannot be longer than 100 characters.")]
         public string Address { get; set; }
+
+        [StringLength(50, ErrorMessage = "City cannot be longer than 50 characters.")]
         public string City { get; set; }
+
+        [StringLength(50, ErrorMessage = "State cannot be longer than 50 characters.")]
         public string State { get; set; }
+
+        [StringLength(10, ErrorMessage = "Zip cannot be longer than 10 characters.")]
         public string Zip { get; set; }
+
         public bool Contract { get; set; }
     }
 }
--- SIA102_Midterms_2/Models/DTO/AuthorDeleteDTO.cs
+++ SIA102_Midterms_2/Models/DTO/AuthorDeleteDTO.cs
@@ -1,7 +1,11 @@
-﻿namespace SIA102_Midterms_2.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SIA102_Midterms_2.DTOs
 {
     public class AuthorDeleteDTO
     {
+        [Required(ErrorMessage = "Author ID is required.")]
+        [StringLength(11, ErrorMessage = "Author ID cannot be longer than 11 characters.")]
         public string AuId { get; set; }
     }
 }
